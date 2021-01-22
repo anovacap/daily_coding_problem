88a88d4c7073c13da58b0fcf5adf1a8bd84d8f77
vagrant <vagrant@localhost.localdomain>
2021-01-22 20:29:00 +0000

    solution for 8.2

--- prefix_map_sum.py
+++ prefix_map_sum.py
@@ -5,6 +5,8 @@
 # methos = sum(grefix: str) - Return the sum of all values of keys that begin
 # with the given prefix.
 
+from collections import defaultdict
+
 class PrefixMapSum:
     def __init__(self):
         self.map = {}
@@ -15,6 +17,53 @@ class PrefixMapSum:
     def sum(self, prefix):
         return sum(value for key, value in self.map.items() if key.startswith(prefix))
 
+class PrefixMapSum_two:
+    def __init__(self):
+        self.map = defaultdict(int)
+        self.words = set()
+    
+    def insert(self, key: str, value: int):
+        # if the key already exists, increment prefix totals
+        # by the difference of old and new values
+        if key in self.words:
+            value -= self.map[key]
+        self.words.add(key)
+
+        for i in range(1, len(key) + 1):
+            self.map[key[:i]] += value
+    def sum(self, prefix):
+        return self.map[prefix]
+
+class TrieNode:
+    def __init__(self):
+        self.letters = {}
+        self.total = 0
+
+class Prefix_map_complex:
+    def __init__(self):
+        self._trie = TrieNode()
+        self.map = {}
+        
+    def insert(self, key, value):
+        # if key exists, increment prefix totals by difference of old and new vals
+        value -= self.map.get(key, 0)
+        self.map[key] = value
+        trie = self._trie
+        for char in key:
+            if char not in trie.letters:
+                trie.letters[char] = TrieNode()
+            trie = trie.letters[char]
+            trie.total += value
+            print("Char = {}, total = {}".format(char, trie.total)) 
+    def sum(self, prefix):
+        d = self._trie
+        for char in prefix:
+            if char in d.letters:
+                d = d.letters[char]
+            else:
+                return "H"
+        return d.total
+
 if __name__ == "__main__":
     prefmapsum = PrefixMapSum()
     prefmapsum.insert("column", 3)
@@ -23,3 +72,16 @@ if __name__ == "__main__":
     prefmapsum.insert("hellos", 22)
     print(prefmapsum.sum("col"))
     print(prefmapsum.sum('hel'))
+    prema2 = PrefixMapSum_two()
+    prema2.insert("column", 3)
+    prema2.insert("hello", 5)
+    prema2.insert("columns", 6)
+    prema2.insert("hellos", 22)
+    print(prema2.sum("col"))
+    print(prema2.sum('hel'))
+    pref_m_c = Prefix_map_complex()
+    pref_m_c.insert('bag', 4)
+    pref_m_c.insert('bath', 5)
+    print("1 = ", pref_m_c.sum('b'))
+    print("2 = ", pref_m_c.sum('ba'))
+    print("3 = ", pref_m_c.sum('bat')) 
